bd562d9622f6db48dcb22219f96276ea1aa50deb
alxayeed <alxayeed@gmail.com>
2025-03-23 14:55:37 +0600

    ADD: fromJson method added in the model with all passing tests

--- lib/features/lendings/data/models/lending_model.dart
+++ lib/features/lendings/data/models/lending_model.dart
@@ -1,3 +1,4 @@
+import 'package:spendly/core/enums/lending_type.dart';
 import 'package:spendly/features/lendings/domain/entity/lend_entity.dart';
 
 class LendingModel extends LendingEntity {
@@ -13,4 +14,21 @@ class LendingModel extends LendingEntity {
     required super.type,
     super.note,
   });
+
+  // fromJson method to parse the JSON map into a LendingModel
+  factory LendingModel.fromJson(Map<String, dynamic> json) {
+    return LendingModel(
+      id: json['id'] as String,
+      amount: json['amount'] as int,
+      date: DateTime.parse(json['date'] as String),
+      dueDate: DateTime.parse(json['dueDate'] as String),
+      lenderId: json['lenderId'] as String,
+      borrowerName: json['borrowerName'] as String,
+      type: LendingType.values.firstWhere(
+        (e) => e.toString().split('.').last == json['type'],
+        orElse: () => LendingType.given,
+      ),
+      note: json['note'] as String?,
+    );
+  }
 }
