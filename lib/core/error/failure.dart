4d1df6cd11ca8df4466f9c4bcfb8c280b20ea3e8
alxayeed <alxayeed@gmail.com>
2025-03-23 12:55:20 +0600

    ADD: Failure implementations added. more tests added

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -10,9 +10,45 @@ abstract class Failure extends Equatable {
 class NetworkFailure extends Failure {
   final String message;
 
-  const NetworkFailure(this.message);
+  const NetworkFailure({required this.message});
 
   @override
-  List<Object?> get props =>
-      [message]; // Refers to the 'message' field of NetworkFailure
+  List<Object?> get props => [message];
+}
+
+class ServerFailure extends Failure {
+  final String message;
+
+  const ServerFailure({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class AuthenticationFailure extends Failure {
+  final String message;
+
+  const AuthenticationFailure({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class ValidationFailure extends Failure {
+  final String field;
+  final String error;
+
+  const ValidationFailure({required this.field, required this.error});
+
+  @override
+  List<Object?> get props => [field, error];
+}
+
+class CacheFailure extends Failure {
+  final String message;
+
+  const CacheFailure({required this.message});
+
+  @override
+  List<Object?> get props => [message];
 }
