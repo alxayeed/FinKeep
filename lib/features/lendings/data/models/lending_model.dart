c3d257cd2e7f6772d10b5850e14c7fb3ca44a7fb
alxayeed <alxayeed@gmail.com>
2025-03-23 14:40:57 +0600

    ADD: lending model added that meets the previous test

--- lib/features/lendings/data/models/lending_model.dart
+++ lib/features/lendings/data/models/lending_model.dart
@@ -1,43 +1,16 @@
-// import '../../../../core/enums/lending_type.dart';
-// import '../../domain/entity/lend_entity.dart';
-//
-// class LendingModel extends LendingEntity {
-//   final String id;
-//
-//   const LendingModel({
-//     required this.id,
-//     required super.amount,
-//     required super.date,
-//     required super.dueDate,
-//     required super.lenderId,
-//     required super.borrowerName,
-//     required super.type,
-//     super.note,
-//   });
-//
-//   factory LendingModel.fromJson(Map<String, dynamic> json) {
-//     return LendingModel(
-//       id: json['id'],
-//       amount: json['amount'],
-//       date: DateTime.parse(json['date']),
-//       dueDate: DateTime.parse(json['dueDate']),
-//       lenderId: json['lenderId'],
-//       borrowerName: json['borrowerName'],
-//       type: LendingType.values.firstWhere((e) => e.toString() == json['type']),
-//       note: json['note'],
-//     );
-//   }
-//
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'id': id,
-//       'amount': amount,
-//       'date': date.toIso8601String(),
-//       'dueDate': dueDate.toIso8601String(),
-//       'lenderId': lenderId,
-//       'borrowerName': borrowerName,
-//       'type': type.toString(),
-//       'note': note,
-//     };
-//   }
-// }
+import 'package:spendly/features/lendings/domain/entity/lend_entity.dart';
+
+class LendingModel extends LendingEntity {
+  final String id;
+
+  const LendingModel({
+    required this.id,
+    required super.amount,
+    required super.date,
+    required super.dueDate,
+    required super.lenderId,
+    required super.borrowerName,
+    required super.type,
+    super.note,
+  });
+}
